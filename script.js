9fec049a29bae63e87b44a8bce27e065cfbafd1a
Hein Lu Lu <heinlulu96@gmail.com>
2026-01-01 19:02:39 +0400

    push

--- script.js
+++ script.js
@@ -88,4 +88,15 @@
                 }
             });
         });
-   
+        
+  const header = document.getElementById("site-header");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      header.classList.remove("bg-transparent");
+      header.classList.add("bg-white", "shadow-md");
+    } else {
+      header.classList.remove("bg-white", "shadow-md");
+      header.classList.add("bg-transparent");
+    }
+  });        
