41b24ff2c63c4803a7cfd38f892a9b4c7dbfd110
Hein Lu Lu <heinlulu96@gmail.com>
2026-01-02 18:59:29 +0000

    push

--- script.js
+++ script.js
@@ -48,7 +48,7 @@
             
             try {
                 // Replace with your Cloudflare Worker URL
-                const response = await fetch('YOUR_CLOUDFLARE_WORKER_URL', {
+                const response = await fetch('https://script.google.com/macros/s/AKfycby5jiqm5wScFtvJT59DEEQnuaruNiXxugjmJiTxGUUP09-IvVLfinsyoxYs4leFKXXXYA/exec', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
